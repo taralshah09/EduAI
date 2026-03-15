5f43d28403236a6965d2796a15ba69d9279ca774
Taral Shah <taralshah992005@gmail.com>
2026-03-16 02:47:46 +0530

    add: cors enabled

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://do-tldr.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
