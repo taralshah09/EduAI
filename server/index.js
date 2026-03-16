5daf9bd14065740e65a592bb3b091c7b1be3e5ed
Taral Shah <taralshah992005@gmail.com>
2026-03-16 19:42:08 +0530

    edit: cors enabled for dotldr.app

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://do-tldr.vercel.app"],
+    origin: ["http://localhost:5173", "https://do-tldr.vercel.app", "https://dotdlr.app", "https://www.dotdlr.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
